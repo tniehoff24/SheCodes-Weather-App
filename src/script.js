7ce0c3b2b2861700780bd5d8d91b0d0d3ada8677
Tess <tniehoff24@gmail.com>
2020-09-28 21:56:00 -0400

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,120 @@
+function formatDate(date) {
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  let hours = date.getHours();
+  if (hours > 12) {
+    hours = `${hours - 12}:${minute} PM`;
+  } else {
+    hours = `${hours}:${minute} AM`;
+  }
+  let number = date.getDate();
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
+  let monthIndex = date.getMonth();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  let month = months[monthIndex];
+
+  return `${day}, ${number} ${month}  at ${hours}`;
+}
+
+function showTemperature(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#today-temp").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
+    response.data.wind.speed
+  )} Knots`;
+  document.querySelector(
+    "#condition"
+  ).innerHTML = `Conditions: ${response.data.weather[0].main}`;
+}
+
+function showCityTemp(city) {
+  let apiKey = "bf71e32409a6d0dde84d4b8e435cc431";
+  let tempUnits = "metric";
+  let apiCityLink = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${tempUnits}`;
+  axios.get(apiCityLink).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#text-input-city").value;
+  showCityTemp(city);
+}
+
+function showGeoTemp(position) {
+  let apiKey = "bf71e32409a6d0dde84d4b8e435cc431";
+  let tempUnits = "metric";
+  let longitude = position.coords.longitude;
+  let latitude = position.coords.latitude;
+  let apiGeoLink = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${tempUnits}`;
+  axios.get(apiGeoLink).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showGeoTemp);
+}
+
+function changeCelsius(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#today-temp");
+  currentTemp.innerHTML = "19°";
+}
+
+function changeFahrenheit(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#today-temp");
+  currentTemp.innerHTML = "66°";
+}
+
+//Feature 1: Display current Date and Time
+let dateElement = document.querySelector("#date");
+let now = new Date();
+dateElement.innerHTML = formatDate(now);
+
+//Feature 2: Add search engine and display name after user submits the form
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+//Feature 3: Display fake temperature (16/66) that converts when you click on celsius or Fareignheit
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", changeFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", changeCelsius);
+
+//Feature 4: Searching for Temperature
+let refreshButton = document.querySelector("#refresh-button");
+refreshButton.addEventListener("click", getCurrentLocation);
+
+showCityTemp("Baltimore");
